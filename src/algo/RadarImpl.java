747cb3e79f4713e68f4b7cd8867a7afb85bb3678
3530508 <3530508@ppti-14-303-04.ufr-info-p6.jussieu.fr>
2019-02-08 14:51:21 +0100

    modifs radars

--- src/algo/RadarImpl.java
+++ src/algo/RadarImpl.java
@@ -10,44 +10,58 @@ public class RadarImpl implements Radar {
 	private Voiture voiture;
 	private Circuit circuit;
 	private double[] angles;
+	private double[] distPix;
+	private int BestIndex;
 	
 	public RadarImpl() {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public double[] scores() {
-		
-		return null;
+	public int[] scores(double epsilon) {
+		int[] scores;
+		int taille=angles.length;
+		scores=new int[taille];
+		double max = 0;
+		int imax = 0;
+
+		for (int i=0;i<taille;i++) {
+			scores[i]=calcScore(angles[i],epsilon);
+			if(scores[i]>=max) {
+				max = scores[i];
+				imax = i;
+			}
+			distPix[i]=scores[i]*epsilon;
+		}
+		this.BestIndex = imax;
+		return scores;
 	}
 
 	@Override
 	public double[] distancesInPixels() {
-		// TODO Auto-generated method stub
-		return null;
+		return distPix;
 	}
 
 	@Override
 	public int getBestIndex() {
-		// TODO Auto-generated method stub
-		return 0;
+		return BestIndex;
 	}
 
 	@Override
 	public double[] thetas() {
 		// TODO Auto-generated method stub
-		return null;
+		return angles;
 	}
 	
 	
 	
-	private int calcScore(double epsilon) {
+	private int calcScore(double angle,double epsilon) {
 		Vecteur p = voiture.getPosition();
-		Vecteur d = voiture.getDirection();
+		Vecteur direction = voiture.getDirection().rotation(angle);
 		int cpt=0;
 		while ((TerrainTools.charFromTerrain(circuit.getTerrain(p)) !='g')&&(circuit.estDansCircuit(p))) {
 			cpt++;
-			p.addition(d.multiplication(epsilon));
+			p.addition(direction.multiplication(epsilon));
 		}
 		return cpt;
 	}
