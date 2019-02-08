e2a859325140b4e70cd4797b585533a65557bdb8
3530508 <3530508@ppti-14-303-04.ufr-info-p6.jussieu.fr>
2019-02-08 14:51:33 +0100

    modifs radars

--- src/algo/Radar.java
+++ src/algo/Radar.java
@@ -1,7 +1,7 @@
 package algo;
 
 public interface Radar {
-	public double[] scores(); // score de chaque branche
+	public int[] scores(double epsilon); // score de chaque branche
     public double[] distancesInPixels(); // pour l'observer
     public int getBestIndex(); // meilleur indice
     public double[] thetas(); // angles de chaque faisceau
