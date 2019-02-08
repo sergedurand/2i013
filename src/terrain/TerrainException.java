c90c3f7175c7994b7ad9f42db051e8022c460c6c
sergedurand <46579305+sergedurand@users.noreply.github.com>
2019-02-08 11:27:17 +0100

    terrain update

--- src/terrain/TerrainException.java
+++ src/terrain/TerrainException.java
@@ -7,6 +7,6 @@ public class TerrainException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public TerrainException(String message) {
-		super("Problème terrain : "+message);
+		super("Probleme terrain : "+message);
 	}
 }
